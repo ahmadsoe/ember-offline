d6c0c0c1897820b98230db397ec41f9aa4c4a6eb
Devonte <devo.tox.89@gmail.com>
2017-08-29 22:54:46 +0100

    Fix for fastboot

--- app/initializers/offline.js
+++ app/initializers/offline.js
@@ -4,6 +4,7 @@ export function initialize() {
   const application = arguments[1] || arguments[0];
 
   // apply config to global Offline
+  window.Offline = window.Offline || {};
   window.Offline.options = config.emberOffline;
 
   application.register('offline:main', window.Offline, { instantiate: false });
